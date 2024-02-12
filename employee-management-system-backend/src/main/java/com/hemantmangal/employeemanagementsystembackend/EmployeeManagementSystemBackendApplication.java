ff50b03061e9a8aca63a14fbff87267dc0e5094a
MangalHemant <hemantmangal22@gmail.com>
2024-02-13 02:10:27 +0530

    Initial commit

---
+++ employee-management-system-backend/src/main/java/com/hemantmangal/employeemanagementsystembackend/EmployeeManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hemantmangal.employeemanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ employee-management-system-backend/src/test/java/com/hemantmangal/employeemanagementsystembackend/EmployeeManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemantmangal.employeemanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
