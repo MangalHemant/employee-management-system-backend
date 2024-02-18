f0db06061166bc94c681368abe22c4bdf83632db
MangalHemant <hemantmangal22@gmail.com>
2024-02-18 20:11:40 +0530

    commit

--- employee-management-system-backend/src/main/java/com/hemantmangal/employeemanagementsystembackend/service/EmployeeService.java
+++ employee-management-system-backend/src/main/java/com/hemantmangal/employeemanagementsystembackend/service/EmployeeService.java
@@ -11,4 +11,8 @@ public interface EmployeeService {
     List<Employee> getAllEmployees();
 
     boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
 }
